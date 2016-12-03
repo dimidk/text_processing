813101f6e1c8b5e04b3e2aaf245e7643edc9489d
boys <boys@boys-ThinkPad-X201.(none)>
2016-12-03 19:08:58 +0200

    find file and return filename

--- __init__.py
+++ __init__.py
@@ -3,6 +3,40 @@
 
 import string
 from nltk.corpus import stopwords
+import os.path
+
 
 punctuation=list(string.punctuation)
 stop_words=set(stopwords.words('english'))
+resultFile="results_"
+commonFilename="commonFile_"
+
+tweetsFile=commonFilename+"tweets.txt"
+wikiFile=commonFilename+"wikipedia.txt"
+darwinFile=commonFilename+"darwin.txt"
+	
+def findFile():
+	
+	filenum=[]
+	
+	
+	heapFileTweets=resultFile+"tweets.txt"
+	heapFileWiki=resultFile+"wikipedia.txt"
+	
+	if os.path.isfile(tweetsFile):
+		filenum.append('1')
+	else:
+		filenum.append('0')
+	
+	if os.path.isfile(wikiFile):
+		filenum.append('1')
+	else:
+		filenum.append('0')
+	
+	if os.path.isfile(darwinFile):
+		filenum.append('1')
+	else:
+		filenum.append('0')
+	
+	return filenum
+	
