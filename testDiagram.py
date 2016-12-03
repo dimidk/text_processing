9b66c311bfad79b25c10df24a4cbdfcbf87319cc
boys <boys@boys-ThinkPad-X201.(none)>
2016-12-03 19:08:18 +0200

    make two lines diagram

--- testDiagram.py
+++ testDiagram.py
@@ -2,12 +2,14 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import __init__
+
 
 def plotCommonWords(fname,number):
 	
-	filename="commonFile_"+fname
+	"""filename=__init__.commonFilename+fname"""
 	
-	with open(filename,"r") as fh:
+	with open(fname,"r") as fh:
 		line=fh.read()
 	data=line.split('\n')
 	y=[]
@@ -32,23 +34,41 @@ def plotHeapDiagram(fname):
 	
 	y=[]
 	x=[]
-	
+	x_20=[]
+	y_20=[]
 	with open(fname,"r") as fh:
 		lines=fh.readlines()
 		
+	lineNum=0
 	for line in lines:
 		if line.startswith('File') or line.startswith('Number'):
 			continue
 		line=line.split('\t')
-		x.append(int(line[1]))
-		y.append(int(line[0]))
-	
+		if lineNum%2==0:
+			x.append(int(line[1]))
+			y.append(int(line[0]))
+		
+		else:
+			x_20.append(int(line[1]))
+			y_20.append(int(line[0]))
+		lineNum+=1
+		
 	x.pop(len(x)-1)
-	y.pop(len(x)-1)				
-	plt.plot(x,y)
+	y.pop(len(y)-1)
+	x=x+x_20
+	y=y+y_20
+		
+	plt.plot(x,y,label='10collection')
+	plt.plot(x_20,y_20,marker='.',linestyle='--',color='red',label='20 collection')
 	plt.title("Heaps Diagram")
 	plt.xlabel('Word List in Document	')
 	plt.ylabel('Unique Word in Dictionary')
+	plt.show()
+	
+	"""plt.plot(x_20,y_20)
+	plt.title("Heaps Diagram")
+	plt.xlabel('Word List in Document	')
+	plt.ylabel('Unique Word in Dictionary')"""
 	plt.show()
 	
 if __name__== '__main__':
